adeeca50642531aec9d2c5e08217e1b41f417568
Elad Gil <elad@helleko.com>
2020-04-06 18:36:38 +0300

    better closing logic

--- zoomkiller.js
+++ zoomkiller.js
@@ -1,5 +1,7 @@
 chrome.webNavigation.onCompleted.addListener((details) => {
-    chrome.tabs.remove(details.tabId, () => {
-        console.log('TAB closed!');
-    });
+    chrome.tabs.remove(details.tabId, () => {});
 }, {url: [{urlMatches : 'https://zoom.us/postattendee'}]});
+
+chrome.webNavigation.onCompleted.addListener((details) => {
+    chrome.tabs.remove(details.tabId, () => {});
+}, {url: [{hostEquals : 'zoom.us', queryPrefix: 'status=success'}]});
